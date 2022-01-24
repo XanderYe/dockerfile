7c39a9e45581236228f6c6ca2d036f68d932144e
XanderYe <XanderYe@outlook.com>
2022-01-24 10:27:04 +0800

    chore: 修改Base镜像和启动脚本

--- mysql-5.7/docker-entrypoint.sh
+++ mysql-5.7/docker-entrypoint.sh
@@ -1,23 +1,60 @@
 #!/bin/bash
 
-function initMysql() {
-    rm -rf /var/lib/mysql/*
-    mysqld --initialize --user=mysql
-    service mysqld start --skip-grant-tables
-    mysql -uroot << EOF
-flush privileges;
-grant all privileges on *.* to 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD';
-grant all privileges on *.* to 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD';
-flush privileges;
-EOF
-    service mysqld stop
-    echo "mysql initialized successfully"
-}
-
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "start initializing mysql."
-    initMysql
+if [ "${1:0:1}" = '-' ]; then
+    set -- mysqld "$@"
 fi
 
-service mysqld start
-tail -f /dev/null
+if [ "$1" = "mysqld" ]; then
+  if [ ! -d "$DATADIR/mysql" ]; then
+    echo "start initializing MySQL."
+
+    mkdir -p "$DATADIR"
+    chown -R mysql:mysql "$DATADIR"
+
+    # 初始化MySQL
+    mysqld --initialize-insecure --user=mysql --datadir="$DATADIR"
+    # 启动MySQL
+    mysqld --user=mysql &
+    # 获取进程id
+    pid="$!"
+
+    mysql=( mysql --protocol=socket -uroot )
+
+    # 检测mysql是否启动
+    for i in {30..0}; do
+        if echo 'SELECT 1' | "${mysql[@]}" &> /dev/null; then
+            break
+        fi
+        echo 'MySQL init process in progress...'
+        sleep 1
+    done
+    if [ "$i" = 0 ]; then
+        echo >&2 'MySQL init process failed.'
+        exit 1
+    fi
+
+    # 配置用户和密码
+    "${mysql[@]}" -uroot <<-EOSQL
+FLUSH PRIVILEGES ;
+DELETE FROM mysql.user;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;
+FLUSH PRIVILEGES ;
+EOSQL
+    if ! kill -s TERM "$pid" || ! wait "$pid"; then
+        echo >&2 'MySQL init process failed.'
+        exit 1
+    fi
+
+    echo
+    echo 'MySQL init process done. Ready for start up.'
+    echo
+  fi
+  chown -R mysql:mysql "$DATADIR"
+
+  if [ "$(id -u)" = "0" ]; then
+    echo "Switching to dedicated user 'mysql'"
+      exec gosu mysql "$BASH_SOURCE" "$@"
+  fi
+fi
+
+exec "$@"
